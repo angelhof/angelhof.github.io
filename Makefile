d54cbcab6bb2584d7a4641d5310a177bd6d360e5
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2024-08-21 15:59:58 +0300

    books

--- Makefile
+++ Makefile
@@ -4,11 +4,13 @@ PHONY: static
 static:
 	python3 bib2html.py
 	python3 escape_rooms.py
+	python3 book_parser.py ../../Personal/thoughts/books.csv
 	@(cd cv && make)
 	cp cv/cv.pdf files/cv_konstantinos_kallas.pdf
 
 static-debug:
 	python3 bib2html.py --no-check
 	python3 escape_rooms.py
+	python3 book_parser.py ../../Personal/thoughts/books.csv
 	@(cd cv && make)
 	cp cv/cv.pdf files/cv_konstantinos_kallas.pdf
---
+++ book_parser.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+
+from datetime import datetime
+
+def export(filename, content):
+    with open(filename, 'w') as html_file:
+        html_file.write(content)
+
+def convert_date(date_str):
+    # Parse the input date string
+    date_obj = datetime.strptime(date_str, "%m/%Y")
+    
+    # Format the date to "mmmm YYYY"
+    formatted_date = date_obj.strftime("%B %Y")
+    
+    return formatted_date
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+                    prog='Book Parser',
+                    description='Parses a books csv to produce an html page')
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    return args
+
+
+def parse_book_file(filename):
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file.readlines()]
+
+    data = []
+    for line in lines:
+        words = line.split('|')
+        item = {}
+        item['title'] = words[0].strip()
+        item['author'] = words[1].strip()
+        item['date'] = words[2].strip()
+        item['location'] = words[3].strip()
+        if len(words) == 5 and words[4].strip() == '*':
+            item['starred'] = True
+        else:
+            item['starred'] = False
+        data.append(item)
+    return data
+
+
+def format_books(data):
+    output_html = ""
+    output_html += f'<ul>\n'
+    for item in data:
+        output_html += generate_book_html(item)
+    output_html += "</ul>\n"
+    return output_html
+
+def generate_book_html(item):
+    output = f'<li> {item["title"]} -- {item["author"]} -- {convert_date(item["date"])}'
+    if item['starred']:
+        output += ' <b>*</b>'
+    output += '</li>\n'
+    return output
+
+args = parse_args()
+data = parse_book_file(args.filename)
+output_html = format_books(data)
+export('book-list.html', output_html)
